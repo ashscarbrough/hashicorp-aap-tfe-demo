f989a924fcd048c12d9337af08e1da4c32d0499a
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-04-02 15:48:19 -0400

    fix: resolve initial errors for missing resource

--- iam.tf
+++ iam.tf
@@ -23,73 +23,9 @@ resource "aws_iam_role_policy_attachment" "ec2_modify_metadata" {
   policy_arn = aws_iam_policy.ec2_modify_metadata.arn
 }
 
-# Get parameters from the SSM Parameter Store.
-
-data "aws_iam_policy_document" "tfe_get_parameters" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "ssm:DescribeParameters",
-      "ssm:GetParameter",
-      "ssm:GetParametersByPath"
-    ]
-    resources = [
-      aws_ssm_parameter.tfe_version.arn,
-      aws_ssm_parameter.postgresql_major_version.arn,
-      aws_ssm_parameter.tfe_encryption_password.arn,
-      aws_ssm_parameter.tfe_hostname.arn,
-      aws_ssm_parameter.tfe_license.arn,
-      aws_ssm_parameter.tfe_database_host.arn,
-      aws_ssm_parameter.tfe_database_name.arn,
-      aws_ssm_parameter.tfe_database_user.arn,
-      aws_ssm_parameter.tfe_database_password.arn,
-      aws_ssm_parameter.tfe_redis_host.arn,
-      aws_ssm_parameter.tfe_redis_password.arn,
-      aws_ssm_parameter.tfe_object_storage_s3_region.arn,
-      aws_ssm_parameter.tfe_object_storage_s3_bucket.arn,
-    ]
-  }
-}
-
-resource "aws_iam_policy" "tfe_get_parameters" {
-  name   = "SSMReadTerraformEnterpriseParameters"
-  path   = "/"
-  policy = data.aws_iam_policy_document.tfe_get_parameters.json
-}
-
-resource "aws_iam_role_policy_attachment" "tfe_get_parameters" {
-  role       = aws_iam_role.tfe.name
-  policy_arn = aws_iam_policy.tfe_get_parameters.arn
-}
-
-# Put parameters into the SSM Parameter Store.
-
-data "aws_iam_policy_document" "tfe_put_parameters" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "ssm:PutParameter"
-    ]
-    resources = [
-      aws_ssm_parameter.tfe_admin_token_url.arn,
-    ]
-  }
-}
-
-resource "aws_iam_policy" "tfe_put_parameters" {
-  name   = "SSMWriteTerraformEnterpriseAdminTokenURLParameter"
-  path   = "/"
-  policy = data.aws_iam_policy_document.tfe_put_parameters.json
-}
-
-resource "aws_iam_role_policy_attachment" "tfe_put_parameters" {
-  role       = aws_iam_role.tfe.name
-  policy_arn = aws_iam_policy.tfe_put_parameters.arn
-}
-
 # Get secrets from Secrets Manager.
 
-data "aws_iam_policy_document" "tfe_secrets_manager" {
+data "aws_iam_policy_document" "aap_tfe_demo_secrets_manager" {
   statement {
     effect = "Allow"
     actions = [
@@ -99,7 +35,7 @@ data "aws_iam_policy_document" "tfe_secrets_manager" {
       "secretsmanager:ListSecretVersionIds"
     ]
     resources = [
-      aws_db_instance.tfe.master_user_secret[0].secret_arn
+      aws_db_instance.aap_tfe_demo.master_user_secret[0].secret_arn
     ]
   }
   statement {
@@ -111,60 +47,26 @@ data "aws_iam_policy_document" "tfe_secrets_manager" {
   }
 }
 
-resource "aws_iam_policy" "tfe_secrets_manager" {
-  name   = "SecretsManagerReadTerraformEnterpriseSecrets"
+resource "aws_iam_policy" "aap_tfe_demo_secrets_manager" {
+  name   = "SecretsManagerReadAAPTFESecrets"
   path   = "/"
-  policy = data.aws_iam_policy_document.tfe_secrets_manager.json
+  policy = data.aws_iam_policy_document.aap_tfe_demo_secrets_manager.json
 }
 
-resource "aws_iam_role_policy_attachment" "tfe_secrets_manager" {
-  role       = aws_iam_role.tfe.name
-  policy_arn = aws_iam_policy.tfe_secrets_manager.arn
-}
-
-# Allow full access to the TFE S3 bucket.
-
-data "aws_iam_policy_document" "tfe_s3" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:ListBucket"
-    ]
-    resources = [
-      aws_s3_bucket.tfe.arn
-    ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:*Object"
-    ]
-    resources = [
-      "${aws_s3_bucket.tfe.arn}/*"
-    ]
-  }
-}
-
-resource "aws_iam_policy" "tfe_s3" {
-  name   = "S3WriteTerraformEnterpriseBucket"
-  path   = "/"
-  policy = data.aws_iam_policy_document.tfe_s3.json
-}
-
-resource "aws_iam_role_policy_attachment" "tfe_s3" {
-  role       = aws_iam_role.tfe.name
-  policy_arn = aws_iam_policy.tfe_s3.arn
+resource "aws_iam_role_policy_attachment" "aap_tfe_demo_secrets_manager" {
+  role       = aws_iam_role.aap_tfe_demo.name
+  policy_arn = aws_iam_policy.aap_tfe_demo_secrets_manager.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
-  role       = aws_iam_role.tfe.name
+  role       = aws_iam_role.aap_tfe_demo.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 
-# Create an EC2 instance profile using the TFE role.
+# Create an EC2 instance profile using the aap_tfe_demo role.
 
-data "aws_iam_policy_document" "tfe_assume_role" {
+data "aws_iam_policy_document" "aap_tfe_demo_assume_role" {
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
@@ -177,14 +79,14 @@ data "aws_iam_policy_document" "tfe_assume_role" {
   }
 }
 
-resource "aws_iam_role" "tfe" {
+resource "aws_iam_role" "aap_tfe_demo" {
   name               = var.ec2_iam_role_name
   path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.tfe_assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.aap_tfe_demo_assume_role.json
 }
 
-resource "aws_iam_instance_profile" "tfe" {
+resource "aws_iam_instance_profile" "aap_tfe_demo" {
   name = var.ec2_instance_profile_name
   path = "/"
-  role = aws_iam_role.tfe.name
+  role = aws_iam_role.aap_tfe_demo.name
 }
--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,16 @@
-output "tfe_hostname" {
-  description = "Fully qualified domain name (FQDN) of the Terraform Enterprise endpoint."
-  value       = aws_route53_record.alias_record.fqdn
-}
+# output "tfe_hostname" {
+#   description = "Fully qualified domain name (FQDN) of the Terraform Enterprise endpoint."
+#   value       = aws_route53_record.alias_record.fqdn
+# }
 
-output "alb_dns_name" {
-  description = "DNS name of the Application Load Balancer (ALB) that fronts Terraform Enterprise."
-  value       = aws_lb.tfe.dns_name
-}
-
-output "asg_name" {
-  description = "Name of the Auto Scaling group for Terraform Enterprise instances."
-  value       = aws_autoscaling_group.tfe.name
-}
-
-output "rds_endpoint" {
-  description = "RDS endpoint hostname for the PostgreSQL database."
-  value       = aws_db_instance.tfe.address
-}
-
-output "elasticache_primary_endpoint" {
-  description = "Primary endpoint address of the ElastiCache Redis replication group."
-  value       = aws_elasticache_replication_group.tfe.primary_endpoint_address
-}
-
-output "s3_bucket_id" {
-  description = "Name (ID) of the S3 bucket used for Terraform Enterprise object storage."
-  value       = aws_s3_bucket.tfe.id
-}
+# output "alb_dns_name" {
+#   description = "DNS name of the Application Load Balancer (ALB) that fronts Terraform Enterprise."
+#   value       = aws_lb.tfe.dns_name
+# }
 
 output "security_group_ids" {
-  description = "Map of security group IDs keyed by component: tfe, alb."
+  description = "Map of security group IDs keyed by component: aap_tfe_demo."
   value = {
-    tfe = aws_security_group.tfe.id
-    alb = aws_security_group.alb.id
+    aap_tfe_demo = aws_security_group.aap_tfe_demo.id
   }
 }
