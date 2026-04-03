69c97e0e3e461d3c13671c5e4191d982a3f8f2f8
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-04-03 13:07:29 -0400

    fix: update values to align TF and ansible configurations

--- ansible.tf
+++ ansible.tf
@@ -1,18 +1,19 @@
 # Ansible Automation Platform (AAP) resources to manage the EC2 host inventory and provisioning job.
 
 resource "aap_host" "ec2" {
-  name         = aws_instance.main.public_dns
+  name         = aws_instance.aap_tfe_demo_host.public_dns
   inventory_id = var.aap_inventory_id
   variables    = jsonencode({
-    ansible_host             = aws_instance.main.public_ip
-    ansible_user             = "ec2-user"  # The default user for Amazon Linux 2 AMIs is "ec2-user". Adjust if using a different AMI.
-    ansible_ssh_private_key  = data.aws_secretsmanager_secret_version.ec2_private_key.secret_string  # Private key will be pulled from Secrets Manager at runtime
-    ansible_ssh_common_args  = "-o StrictHostKeyChecking=no"  # the EC2 instance is brand new on each apply, so we need to disable strict host key checking
+    ansible_host            = aws_instance.aap_tfe_demo_host.public_ip
+    ansible_user            = "ec2-user" # Amazon Linux default user.
+    ansible_ssh_private_key = data.aws_secretsmanager_secret_version.aap_tfe_demo_host_private_key.secret_string
+    ansible_ssh_common_args = "-o StrictHostKeyChecking=no" # Host is replaced frequently.
   })
 }
 
-data "aws_secretsmanager_secret_version" "ec2_private_key" {
-  secret_id = aws_secretsmanager_secret.ec2_private_key.id
+data "aws_secretsmanager_secret_version" "aap_tfe_demo_host_private_key" {
+  secret_id  = aws_secretsmanager_secret.aap_tfe_demo_host_private_key.id
+  depends_on = [aws_secretsmanager_secret_version.aap_tfe_demo_host_private_key]
 }
 
 resource "aap_job" "provision" {
@@ -20,11 +21,11 @@ resource "aap_job" "provision" {
   inventory_id    = var.aap_inventory_id
 
   extra_vars = jsonencode({
-    target_host = aws_instance.main.public_ip
+    target_host = aws_instance.aap_tfe_demo_host.public_ip
   })
 
   depends_on = [
     aap_host.ec2,
-    aws_instance.main
+    aws_instance.aap_tfe_demo_host
   ]
 }
