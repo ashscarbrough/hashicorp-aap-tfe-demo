49027caa4e135a0e348646c3f44e0eaf6b97685d
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-04-07 14:37:02 -0400

    fix: update AAP instance group values and ec2 code layout

--- ec2.tf
+++ ec2.tf
@@ -1,27 +1,6 @@
-resource "aws_instance" "aap_tfe_demo_host" {
-  ami                  = local.ami_id
-  instance_type        = local.ec2_instance_type
-  key_name             = aws_key_pair.aap_tfe_demo_host.key_name
-
-  user_data            = file(local.user_data_script)
-  monitoring           = true
-
-  iam_instance_profile = aws_iam_instance_profile.aap_tfe_demo.name
-
-  associate_public_ip_address = true
-  subnet_id            = var.ec2_subnet_id
-  vpc_security_group_ids = [aws_security_group.aap_tfe_demo.id]
-
-  lifecycle {
-    # SSH keys are injected at instance launch. Recreate the instance if key material changes.
-    replace_triggered_by = [aws_key_pair.aap_tfe_demo_host]
-  }
-
-  tags = {
-    Name = var.ec2_instance_name
-    environment = var.environment
-  }
-}
+# TLS private key resource to generate SSH key pair for EC2 instance access. 
+# The private key will be stored securely in AWS Secrets Manager, and the 
+# public key will be used to create an AWS Key Pair for the EC2 instance.
 
 resource "tls_private_key" "aap_tfe_demo_host_key" {
   algorithm = "RSA"
@@ -44,6 +23,31 @@ resource "aws_secretsmanager_secret_version" "aap_tfe_demo_host_private_key" {
   secret_string = tls_private_key.aap_tfe_demo_host_key.private_key_openssh
 }
 
+
+# AWS EC2 instanc
+resource "aws_instance" "aap_tfe_demo_host" {
+  ami                  = local.ami_id
+  instance_type        = local.ec2_instance_type
+  key_name             = aws_key_pair.aap_tfe_demo_host.key_name
+
+  user_data            = file(local.user_data_script)
+  monitoring           = true
+
+  iam_instance_profile = aws_iam_instance_profile.aap_tfe_demo.name
+
+  associate_public_ip_address = true
+  subnet_id            = var.ec2_subnet_id
+  vpc_security_group_ids = [aws_security_group.aap_tfe_demo.id]
+
+  lifecycle {
+    # SSH keys are injected at instance launch. Recreate the instance if key material changes.
+    replace_triggered_by = [aws_key_pair.aap_tfe_demo_host]
+  }
+
+  tags = {
+    Name = var.ec2_instance_name
+  }
+}
 
 # Resource to wait for cloud-init to complete on the EC2 instance before allowing Ansible provisioning to proceed. 
 # This ensures that the instance is fully initialized and ready to accept SSH connections before we attempt to run any Ansible tasks against it.
