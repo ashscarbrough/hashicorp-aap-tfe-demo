cb9c0695752f0089cc56ca64ea42742c3adee4eb
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-04-08 14:24:06 -0400

    fix: Test run AAP to see if playbook mitigates errors

--- actions.tf
+++ actions.tf
@@ -1,25 +1,5 @@
 
 # Define an action to send a payload to AAP API.
-# # 
-# action "aap_job_launch" "run_playbook" {
-#   config {
-#     url    = "${var.aap_host}/api/controller/v2/job_templates/${var.aap_job_template_id}/launch/"
-#     method = "POST"
-#     request_headers = {
-#       Authorization = "Bearer ${var.aap_token}"
-#       Content-Type  = "application/json"
-#     }
-#     request_body = jsonencode({
-#       inventory  = var.aap_inventory_id
-#       extra_vars = {
-#         target_host = aws_instance.main.public_ip
-#       }
-#     })
-#   }
-# }
-
-
-# Define an action to send a payload to AAP API.
 action "aap_job_launch" "run_playbook" {
   config {
     job_template_id     = var.aap_tf_actions_job_template_id
