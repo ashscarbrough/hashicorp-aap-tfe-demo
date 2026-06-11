3ced4610cca8f6393fe0e33bb4cf71d1c723fcc7
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-06-11 14:29:17 -0400

    fix: change tf actions resource names for plan

--- al-2023/actions.tf
+++ al-2023/actions.tf
@@ -31,7 +31,7 @@ action "aap_job_launch" "rollback_playbook" {
 # the instance must have the appropriate IAM permissions and SSM agent installed.
 # -------------------------------------------------------------
 
-action "aap_job_launch" "run_new_version_playbook_ssm" {
+action "aap_job_launch" "ssm_run_new_version_playbook_ssm" {
   config {
     job_template_id     = 20
     wait_for_completion = true
@@ -48,7 +48,7 @@ action "aap_job_launch" "run_new_version_playbook_ssm" {
 # Rollback action — triggered manually from the HCP Terraform UI.
 # Points at the configure_demo_instance_v1.2.0.yml job template in AAP.
 # The version to restore is determined by the job template itself — no extra vars required.
-action "aap_job_launch" "rollback_playbook" {
+action "aap_job_launch" "ssm_rollback_playbook" {
   config {
     job_template_id     = 21
     wait_for_completion = true
