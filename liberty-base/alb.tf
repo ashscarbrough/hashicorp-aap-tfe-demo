9afdb8251c9c454269bb4ff2c7f02e8d2c86607b
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-06-21 15:53:58 -0400

    fix: alb to ec2 resource names

--- liberty-base/alb.tf
+++ liberty-base/alb.tf
@@ -53,7 +53,7 @@ resource "aws_lb_target_group" "liberty_base" {
 # --- Target Group Attachment ---
 resource "aws_lb_target_group_attachment" "liberty_base" {
   target_group_arn = aws_lb_target_group.liberty_base.arn
-  target_id        = aws_instance.aap_tfe_demo_host.id
+  target_id        = aws_instance.liberty_base_host.id
   port             = 9080
 
   # Terraform won't detach (and therefore won't allow destroy of old instance)
@@ -77,12 +77,12 @@ resource "aws_lb_listener" "liberty_base_http" {
 
 resource "null_resource" "wait_for_alb_healthy" {
   triggers = {
-    instance_id = aws_instance.aap_tfe_demo_host.id
+    instance_id = aws_instance.liberty_base_host.id
   }
 
   provisioner "local-exec" {
     command = <<-EOT
-      echo "Waiting for instance ${aws_instance.aap_tfe_demo_host.id} to become healthy..."
+      echo "Waiting for instance ${aws_instance.liberty_base_host.id} to become healthy..."
       echo "Timeout: 20 minutes (AAP install + Liberty startup)"
 
       ATTEMPTS=0
@@ -91,7 +91,7 @@ resource "null_resource" "wait_for_alb_healthy" {
       until [ $ATTEMPTS -ge $MAX_ATTEMPTS ]; do
         STATE=$(aws elbv2 describe-target-health \
           --target-group-arn ${aws_lb_target_group.liberty_base.arn} \
-          --targets Id=${aws_instance.aap_tfe_demo_host.id} \
+          --targets Id=${aws_instance.liberty_base_host.id} \
           --region ${var.aws_region} \
           --query 'TargetHealthDescriptions[0].TargetHealth.State' \
           --output text)
@@ -113,5 +113,5 @@ resource "null_resource" "wait_for_alb_healthy" {
     EOT
   }
 
-  depends_on = [aws_instance.aap_tfe_demo_host]
+  depends_on = [aws_instance.liberty_base_host]
 }
