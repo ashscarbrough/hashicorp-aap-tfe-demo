86c00341d91d9b1b4a15306d1445c1fc4032af19
Ash Scarbrough <ashley.scarbrough@ibm.com>
2026-06-24 13:25:17 -0400

    fix: update access key values

--- foundational_resources/aap_turbonomic_user.tf
+++ foundational_resources/aap_turbonomic_user.tf
@@ -118,18 +118,3 @@ resource "aws_iam_user_policy" "aap_ssm_sessions" {
 resource "aws_iam_access_key" "aap_integration" {
   user = aws_iam_user.aap_integration.name
 }
-
-# -----------------------------------------------------------------------------
-# Outputs -- reference these when configuring the AAP AWS credential
-# -----------------------------------------------------------------------------
-
-output "aap_iam_access_key_id" {
-  description = "Access key ID for the AAP integration IAM user"
-  value       = aws_iam_access_key.aap_integration.id
-}
-
-output "aap_iam_secret_access_key" {
-  description = "Secret access key for the AAP integration IAM user"
-  value       = aws_iam_access_key.aap_integration.secret
-  sensitive   = true
-}
